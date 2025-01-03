99dd7000ffcf78bd02a6f864d70b9a081d757373
biasmj016 <mjkim031@gmail.com>
2025-01-03 14:12:36 +0900

    로또 클래스 수정

--- src/main/kotlin/com/biasmj/kotlin_example/lotto/Lotto.kt
+++ src/main/kotlin/com/biasmj/kotlin_example/lotto/Lotto.kt
@@ -1,9 +1,8 @@
 package com.biasmj.kotlin_example.lotto
 
-data class Lotto(val numbers: List<Int>) {
+data class Lotto(val numbers: Set<Int>) {
     init {
-        require(numbers.size == 6) { "A Lotto must have exactly 6 numbers." }
-        require(numbers.distinct().size == 6) { "Lotto numbers must be unique." }
+        require(numbers.distinct().size == 6) { "A Lotto is required to have precisely 6 numbers." }
         require(numbers.all { it in 1..45 }) { "Lotto numbers must be in 1-45." }
     }
 }
--- src/test/kotlin/com/biasmj/kotlin_example/lotto/LottoTest.kt
+++ src/test/kotlin/com/biasmj/kotlin_example/lotto/LottoTest.kt
@@ -8,31 +8,23 @@ class LottoTest {
 
     @Test
     fun validCheck_lottos() {
-        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
-        assertEquals(listOf(1, 2, 3, 4, 5, 6), lotto.numbers)
-    }
-
-    @Test
-    fun validCheck_numbers() {
-        val exception = assertThrows<IllegalArgumentException> {
-            Lotto(listOf(1, 2, 3, 4, 5))
-        }
-        assertEquals("A Lotto must have exactly 6 numbers.", exception.message)
-    }
-
-    @Test
-    fun validCheck_invalid_numbers() {
-        val exception = assertThrows<IllegalArgumentException> {
-            Lotto(listOf(0, 2, 3, 4, 5, 46))
-        }
-        assertEquals("Lotto numbers must be in 1-45.", exception.message)
+        val lotto = Lotto(setOf(1, 2, 3, 4, 5, 6))
+        assertEquals(setOf(1, 2, 3, 4, 5, 6), lotto.numbers)
     }
 
     @Test
     fun validCheck_duplicate_numbers() {
         val exception = assertThrows<IllegalArgumentException> {
-            Lotto(listOf(1, 2, 3, 4, 5, 5))
+            Lotto(setOf(1, 2, 3, 4, 5, 5))
         }
-        assertEquals("Lotto numbers must be unique.", exception.message)
+        assertEquals("A Lotto is required to have precisely 6 numbers.", exception.message)
+    }
+
+    @Test
+    fun validCheck_invalid_numbers() {
+        val exception = assertThrows<IllegalArgumentException> {
+            Lotto(setOf(0, 2, 3, 4, 5, 46))
+        }
+        assertEquals("Lotto numbers must be in 1-45.", exception.message)
     }
 }
