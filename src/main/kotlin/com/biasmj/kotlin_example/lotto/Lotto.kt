135963409adb1bd90456bf2644e239773d064e22
biasmj016 <mjkim031@gmail.com>
2025-01-03 14:43:07 +0900

    필요없어진 부분 제거

--- src/main/kotlin/com/biasmj/kotlin_example/lotto/Lotto.kt
+++ src/main/kotlin/com/biasmj/kotlin_example/lotto/Lotto.kt
@@ -1,8 +1,3 @@
 package com.biasmj.kotlin_example.lotto
 
-data class Lotto(val numbers: Set<Int>) {
-    init {
-        require(numbers.distinct().size == 6) { "A Lotto is required to have precisely 6 numbers." }
-        require(numbers.all { it in 1..45 }) { "Lotto numbers must be in 1-45." }
-    }
-}
+data class Lotto(val numbers: Set<Int>) {}
--- src/test/kotlin/com/biasmj/kotlin_example/lotto/LottoTest.kt
+++ src/test/kotlin/com/biasmj/kotlin_example/lotto/LottoTest.kt
@@ -2,29 +2,12 @@ package com.biasmj.kotlin_example.lotto
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 
 class LottoTest {
 
     @Test
-    fun validCheck_lottos() {
+    fun lottos() {
         val lotto = Lotto(setOf(1, 2, 3, 4, 5, 6))
         assertEquals(setOf(1, 2, 3, 4, 5, 6), lotto.numbers)
-    }
-
-    @Test
-    fun validCheck_duplicate_numbers() {
-        val exception = assertThrows<IllegalArgumentException> {
-            Lotto(setOf(1, 2, 3, 4, 5, 5))
-        }
-        assertEquals("A Lotto is required to have precisely 6 numbers.", exception.message)
-    }
-
-    @Test
-    fun validCheck_invalid_numbers() {
-        val exception = assertThrows<IllegalArgumentException> {
-            Lotto(setOf(0, 2, 3, 4, 5, 46))
-        }
-        assertEquals("Lotto numbers must be in 1-45.", exception.message)
     }
 }
